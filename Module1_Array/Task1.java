8f88034526d873c2a31de46cd45ea9d2401c0aca
241801370049-ctrl <241801370049@cutmap.ac.in>
2025-12-19 15:10:31 +0530

    Update Task1.java

--- Module1_Array/Task1.java
+++ Module1_Array/Task1.java
@@ -1,1 +1,23 @@
-code
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        
+        int a=5;
+        System.out.println(a);
+        
+        // static d&i
+        int[] nums={1,3,4,5,7};
+        System.out.println(nums[3]);
+        System.out.println(nums.length);
+        System.out.println(nums[1]);
+        System.out.println(nums[2]); 
+        
+        for(int i=0;i<nums.length;i++){
+            System.out.println(nums[i]);
+        }
+        
+    }
+}
