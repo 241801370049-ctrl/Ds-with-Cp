5ca2ca500fdf48479d20c4a158404bbb76a83846
241801370049-ctrl <241801370049@cutmap.ac.in>
2025-12-20 16:16:15 +0530

    Refactor array initialization and loop structure

--- Module1_Array/Task1.java
+++ Module1_Array/Task1.java
@@ -3,21 +3,32 @@
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Try programiz.pro");
         
-        int a=5;
-        System.out.println(a);
-        
-        // static d&i
         int[] nums={1,3,4,5,7};
+        int[] num = new int[]{1,2,3};
         System.out.println(nums[3]);
         System.out.println(nums.length);
         System.out.println(nums[1]);
-        System.out.println(nums[2]); 
+        System.out.println(nums[2]);
         
-        for(int i=0;i<nums.length;i++){
+        for(int i = 0; i < nums.length; i++){
             System.out.println(nums[i]);
         }
-        
+            
+            String[] Strings={"raghu","sudha","vyshu"};
+            System.out.println(Strings[0]);
+              System.out.println(Strings[1]);
+                System.out.println(Strings[2]);
+                
+         int[] num1 = new int[10];
+         num1[0]=45;
+         num1[1]=23;
+         num1[5]=9874;
+         num1[9]=495;
+         
+         for(int i = 0; i < num1.length; i++){
+            System.out.println(num1[i]);
+        }
+                   
     }
 }
